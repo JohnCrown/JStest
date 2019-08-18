cd7f15c68aa1fd3a382dabcd856539a90037c2e3
JohnCrown <36449663+JohnCrown@users.noreply.github.com>
2019-08-18 19:19:18 +0300

    Task 6

---
+++ Task6/js/main.js
@@ -0,0 +1,52 @@
+'use strict'
+
+let Startbtn = document.getElementById('start'),
+    mainbudjet = document.querySelectorAll('.budget-value'),
+    budgetday = document.querySelectorAll('.daybudget-value'),
+    incomelvl = document.querySelectorAll('.level-value'),
+    mainexpenses = document.querySelectorAll('.expenses-value'),
+    optexpenses = document.querySelectorAll('.optionalexpenses-value'),
+    income = document.querySelectorAll('.income-value'),
+    savingmounth = document.querySelectorAll('.monthsavings-value'),
+    savingyear = document.querySelectorAll('.yearsavings-value'),
+
+
+    // timedata = document.querySelector('.time-data')
+
+
+    yearm = document.querySelectorAll('.year-value'),
+    monthm = document.querySelectorAll('.mount-value'),
+    daym = document.querySelectorAll('.day-value'),
+
+// Кнопки
+
+    buttons = document.getElementsByTagName('button'),
+    accept1 = document.querySelector('.expenses-item-btn'),
+    accept2 = document.querySelector('.optionalexpenses-btn'),
+    count = document.querySelector('.count-budget-btn'),
+
+// Input поля
+
+    optionalexpensesitem = document.querySelectorAll('.optionalexpenses-item'),
+    expensesitem = document.querySelectorAll('.expenses-item'),
+
+
+    checkbox = document.querySelector('#savings'),
+    sum = document.querySelector('#sum'),
+    percent = document.querySelector('#percent');
+
+
+
+
+
+console.log(accept1);
+console.log(accept2);
+console.log(count);
+console.log(expensesitem);
+console.log(optionalexpensesitem);
+console.log(buttons);
+console.log(Startbtn);
+console.log(sum);
+console.log(daym);
+console.log(monthm);
+console.log(yearm);
